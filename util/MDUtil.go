c8c087fe8f83a8d51ce1f3b6c6346d4e1bbbee31
jclee@gmail.com <jclee@gmail.com>
2020-11-19 15:45:37 +0900

    MDUtil 추가
    1. SHA1 , SHA256 알고리즘 추가
    2. TestCase 추가

--- util/MDUtil.go
+++ util/MDUtil.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"crypto/sha1"
+	"crypto/sha256"
 	"errors"
 )
 
@@ -10,16 +12,31 @@ type MDUtil struct {
 
 type digestResult struct {
 	base64Result string
-	digestResult *[] byte
+	digestResult [] byte
 }
 
 func (mdUtil *MDUtil) init(algName string) {
 	mdUtil.algName = algName
 }
 
-func (mdUtil *MDUtil) digest(source [] byte) (digestResult, error) {
+func (mdUtil MDUtil) digest(source [] byte) (digestResult, error) {
 	if mdUtil.algName == "" {
 		return digestResult{}, errors.New("Alg Name is empty")
 	}
-	return digestResult{}, nil
+
+	result := digestResult{}
+	switch mdUtil.algName {
+	case "SHA1":
+		sum := sha1.Sum(source)
+		result.digestResult = sum[:]
+		result.base64Result = Base64Util{}.Encoding(sum[:])
+	case "SHA256":
+		sum256 := sha256.Sum256(source)
+		result.digestResult = sum256[:]
+		result.base64Result = Base64Util{}.Encoding(sum256[:])
+	default:
+		return digestResult{}, errors.New("지원하는 알고리즘이 없습니다.")
+
+	}
+	return result, nil
 }
--- util/MDUtil_test.go
+++ util/MDUtil_test.go
@@ -9,18 +9,45 @@ import (
 func TestMDUtil_SHA1(t *testing.T)  {
 	assert := assert.New(t)
 
-	assert.Equal("", "")
+	mdUtil := MDUtil{}
+	mdUtil.init("SHA1")
+	digest, err := mdUtil.digest([]byte("TEST1234!@#%한글"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	assert.Equal("WYMNINcipVy8OG23lR1X4WJB/3c=", digest.base64Result)
+	base64Util := Base64Util{}
+	decoding, _ := base64Util.Decoding(digest.base64Result)
+	assert.Equal(digest.digestResult, decoding)
+	fileUtil := FileUtil{}
+	fmt.Println(fileUtil.DumpByteArrayToString(digest.digestResult))
 }
 
 func TestMDUtil_SHA256(t *testing.T)  {
 	assert := assert.New(t)
 
 	mdUtil := MDUtil{}
-	mdUtil.init("SHA1")
-	_, err := mdUtil.digest([]byte("Temp"))
+	mdUtil.init("SHA256")
+	digest, err := mdUtil.digest([]byte("Temp"))
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	assert.Equal("", "")
+	fmt.Println(digest.base64Result)
+	assert.Equal("1i9Da54bY46l3qGBZYa0Q9o8vOKB+sWnyObKBC8kNjA=", digest.base64Result)
+	base64Util := Base64Util{}
+	decoding, _ := base64Util.Decoding(digest.base64Result)
+	assert.Equal(digest.digestResult, decoding)
+
+	fileUtil := FileUtil{}
+	fmt.Println(fileUtil.DumpByteArrayToString(digest.digestResult))
+}
+
+func TestMDUtil_SHA256_ERROR_NOALG(t *testing.T) {
+	assert := assert.New(t)
+
+	mdUtil := MDUtil{}
+	mdUtil.init("SHA123")
+	_, err := mdUtil.digest([]byte("Temp"))
+	assert.Error(err)
 }
